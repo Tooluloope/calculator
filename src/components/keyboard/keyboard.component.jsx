6103a992ec48e79df7c034b1be6a50b065bf96fd
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2020-01-22 07:23:08 +0100

    styling

--- src/components/keyboard/keyboard.component.jsx
+++ src/components/keyboard/keyboard.component.jsx
@@ -8,30 +8,30 @@ export const Keyboard = () => {
     return (
         <div className='keyboard'>
             <div className='top-keys'>
-                <i class="fas fa-history fa-2x icon-history"></i>
-                <i class="fas fa-backspace fa-2x icon-delete"></i>
+                <i className="fas fa-history fa-2x icon-history"></i>
+                <i className="fas fa-backspace fa-2x icon-delete"></i>
             </div>
             <div className='lower-keys'>
                 <button className='button margin-3rd' id='C'> C</button>
-                <button className='button margin-3rd' id='()'> ()</button>
-                <button className='button margin-3rd' id='%'> %</button>
-                <button className='button' id='/'> /</button>
+                <button className='button margin-3rd green' id='()'> ()</button>
+                <button className='button margin-3rd green' id='%'> %</button>
+                <button className='button green' id='/'> /</button>
                 <button className='button margin-3rd' id='7'> 7</button>
                 <button className='button margin-3rd' id='8'> 8</button>
                 <button className='button margin-3rd' id='9'> 9</button>
-                <button className='button' id='*'> *</button>
+                <button className='button green' id='*'> *</button>
                 <button className='button margin-3rd' id='4'> 4</button>
                 <button className='button margin-3rd' id='5'> 5</button>
                 <button className='button margin-3rd' id='6'> 6</button>
-                <button className='button' id='-'> -</button>
+                <button className='button green' id='-'> -</button>
                 <button className='button margin-3rd' id='1'> 1</button>
                 <button className='button margin-3rd' id='2'> 2</button>
                 <button className='button margin-3rd' id='3'> 3</button>
-                <button className='button' id='+'> +</button>
+                <button className='button green' id='+'> +</button>
                 <button className='button margin-3rd' id='+/-'> +/-</button>
                 <button className='button margin-3rd' id='0'> 0</button>
                 <button className='button margin-3rd' id='.'> .</button>
-                <button className='button' id='='> =</button>
+                <button className='button equal' id='='> =</button>
 
                 
             
